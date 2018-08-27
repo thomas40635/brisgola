293538a69fda93961de2bc9d98b0bbc29e00a4a3
Thomas <thomaspauldu06@gmail.com>
2018-08-27 11:22:16 +0200

    Build

--- build/default/src/webservices/config.php
+++ build/default/src/webservices/config.php
@@ -1,3 +1,13 @@
 <?php
 
-$PDO = new PDO('mysql:host=mysql-thomas97bb.alwaysdata.net;dbname=thomas97bb_brisgola','164082_brisgola','5B45a3da61');
+try{
+
+    $PDO = new PDO('mysql:host=mysql-thomas97bb.alwaysdata.net;dbname=thomas97bb_brisgola','164082_brisgola','5B45a3da61');
+
+}
+
+catch(PDOException $e){
+
+    echo 'Connexion impossible';
+
+}
