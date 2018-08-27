912124b2af4c891fc160e9fde162875bf08fbb82
Thomas <thomaspauldu06@gmail.com>
2018-08-27 11:09:42 +0200

    Build

--- build/default/src/webservices/config.php
+++ build/default/src/webservices/config.php
@@ -1,15 +1,3 @@
 <?php
 
-try{
-
-    $PDO = new PDO('mysql:host=mysql-thomas97bb.alwaysdata.net;dbname=','thomas97bb_brisgola','5B45a3da61');
-    $PDO->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
-    $PDO->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
-
-}
-
-catch(PDOException $e){
-
-    echo 'Connexion impossible';
-
-}
+$PDO = new PDO('mysql:host=mysql-thomas97bb.alwaysdata.net;dbname=','thomas97bb_brisgola','5B45a3da61');
