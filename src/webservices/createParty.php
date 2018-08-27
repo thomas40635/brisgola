cd38f7bdd72bc041e1a10b23d2213c08eaab48d3
Thomas <thomaspauldu06@gmail.com>
2018-08-27 12:32:42 +0200

    Build

--- src/webservices/createParty.php
+++ src/webservices/createParty.php
@@ -1,7 +1,30 @@
 <?php
 
 require "config.php";
-$test = json_encode($PDO);
-echo $test;
+
+try{
+	echo $_POST['code'];exit;
+    $req = $PDO->prepare("INSERT INTO Party (login, password, email, infos, roles_id) VALUES (:login, :password, :email, :infos, '2')");
+
+    $req->execute(array(
+            "login" => $login, 
+            "password" => $password,
+            "email" => $email,
+            "infos" => $infos
+            ));
+
+    $data = $req->fetchAll();
+        if(count($data)>0){
+            $_SESSION'Auth'] = $data[0];
+            return true;
+    }
+
+}
+
+catch(PDOException $e){
+
+    echo 'Connexion impossible';
+
+}
 
 ?>
