7f66fff0e0ff24420172b32c5404248e867770ec
Thomas <thomaspauldu06@gmail.com>
2018-08-27 11:00:29 +0200

    Build

--- build/default/src/webservices/config.php
+++ build/default/src/webservices/config.php
@@ -2,10 +2,10 @@
 
 try{
 
-    $PDO = new PDO('mysql:host=localhost;dbname=myblog','root','');
+    $PDO = new PDO('mysql:host=mysql-thomas97bb.alwaysdata.net;dbname=','thomas97bb_brisgola','5B45a3da61');
     $PDO->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
     $PDO->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
-    
+
 }
 
 catch(PDOException $e){
