9ada6a1234ec571768fd0400acce261e5c85c398
Thomas <thomaspauldu06@gmail.com>
2018-08-27 11:11:51 +0200

    Build

--- build/default/src/webservices/config.php
+++ build/default/src/webservices/config.php
@@ -1,3 +1,3 @@
 <?php
 
-$PDO = new PDO('mysql:host=mysql-thomas97bb.alwaysdata.net;dbname=','thomas97bb_brisgola','5B45a3da61');
+$PDO = new PDO('mysql:host=mysql-thomas97bb.alwaysdata.net;dbname=thomas97bb_brisgola','164082_brisgola','5B45a3da61');
