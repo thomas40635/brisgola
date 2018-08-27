870e0e6c8a33e55144656c063fea6b756af2a439
Thomas <thomaspauldu06@gmail.com>
2018-08-27 15:14:57 +0200

    Build

--- src/webservices/createPlayer.php
+++ src/webservices/createPlayer.php
@@ -5,8 +5,10 @@ require "config.php";
 try{
 	$request_body = file_get_contents('php://input');
     $json = json_decode($request_body);
-    $json[0] = $code;
-	$json[1] = $playerID;
+    $code = $json["code"];
+	$playerID = $json["playerID"];
+
+    echo $json;exit;
 
     $req = $PDO->prepare("SELECT player1, player2, player3 FROM party WHERE code = :code");
 
