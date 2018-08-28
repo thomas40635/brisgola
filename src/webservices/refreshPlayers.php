a1db43ed80f5b55f273d174a1a1e1184d68b0f8e
Thomas <thomaspauldu06@gmail.com>
2018-08-28 10:07:19 +0200

    Build

--- src/webservices/refreshPlayers.php
+++ src/webservices/refreshPlayers.php
@@ -7,7 +7,21 @@ try{
     $json = json_decode($request_body, true);
 
     $code = $json["code"];
-    echo("LE CODE : ".$code)
+    $playerID = $json["playerID"];
+
+    $req = $PDO->prepare("SELECT player1,player2,player3 FROM Party WHERE code = :code");
+
+    $req->execute(array(
+            "code" => $code
+            ));
+
+    $data = $req->fetchAll();
+    $players = {
+    	'player1' : $data['player1'],
+    	'player2' : $data['player2'],
+    	'player3' : $data['player3']
+    }
+    return $players;
 
 }
 
