c5355d21dbb59cce50a8fcb22de80954484f346e
Thomas <thomaspauldu06@gmail.com>
2018-08-28 10:23:00 +0200

    Build

--- src/webservices/refreshPlayers.php
+++ src/webservices/refreshPlayers.php
@@ -17,9 +17,9 @@ try{
 
     $data = $req->fetchAll();
     $players = [{
-    	'player1' : $data['player1'],
-    	'player2' : $data['player2'],
-    	'player3' : $data['player3']
+    	"player1" : $data['player1'],
+    	"player2" : $data['player2'],
+    	"player3" : $data['player3']
     }];
     return $players;
 
