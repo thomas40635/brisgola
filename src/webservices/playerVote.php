41467251eeb3aca2d1f21df57787002a58028a8a
Thomas <thomaspauldu06@gmail.com>
2018-09-28 00:54:25 +0200

    .

---
+++ src/webservices/playerVote.php
@@ -0,0 +1,24 @@
+<?php
+
+require "config.php";
+
+try{
+	$request_body = file_get_contents('php://input');
+ 	$json = json_decode($request_body, true);
+
+    $game = $json["game"];
+    $pseudo = $json["pseudo"];
+
+    $req = $PDO->prepare("UPDATE player SET vote = 1 WHERE game = :game AND pseudo = :pseudo LIMIT 1");
+
+	$req->execute(array(
+        "game" => $game,
+        "pseudo" => $pseudo
+    ));
+}
+
+catch(PDOException $e){
+    echo 'Connexion impossible';
+}
+
+?>
---
+++ src/webservices/resetVoteMode.php
@@ -0,0 +1,22 @@
+<?php
+
+require "config.php";
+
+try{
+	$request_body = file_get_contents('php://input');
+ 	$json = json_decode($request_body, true);
+
+    $game = $json["game"];
+
+    $req = $PDO->prepare("UPDATE turn SET etat = 0 WHERE game = :game ORDER BY nbr LIMIT 1");
+
+	$req->execute(array(
+        "game" => $game
+    ));
+}
+
+catch(PDOException $e){
+    echo 'Connexion impossible';
+}
+
+?>
